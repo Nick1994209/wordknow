c4bdde7bdca19b140e8e8745e6a40eff2812e22c
nvkorolkov <NVKorolkov@domclick.ru>
2018-03-28 01:08:42 +0300

    edit run scripts

--- application/run_telegram.sh
+++ application/run_telegram.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python3 manage.py telegram > /logs/console_telegram.logs
+cd /code/wordknow/application
+/usr/bin/python3 manage.py telegram > /logs/console_telegram.logs
--- application/run_telegram_tasks.sh
+++ application/run_telegram_tasks.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python3 manage.py telegram_tasks > /logs/console_telegram_tasks.logs
+cd /code/wordknow/application
+/usr/bin/python3 manage.py telegram_tasks > /logs/console_telegram_tasks.logs
